cb382566e0b8a4877006962dc09a06795c663dc7
reaganmunene <reagannmunene@gmail.com>
2023-05-20 14:48:17 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>I'm doing this work on a Saturday of the date 20/05/2023. If there was a world appocalypse who would even know technology this advanced existed? </p>
+    <img src={image} alt="I made this" ></img>
+  </div>;
 }
 
 export default About;
